6232beae24f40014b9edf69bbfcf31d39974610b
gamerman123x <19superguy98@gmail.com>
2015-02-02 19:57:53 -0500

    Property values

--- config/common.mk
+++ config/common.mk
@@ -246,7 +246,27 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
 
-# BeanStalkGM Extras
+# BlissPopGM Extras
+
+# property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=24 \
+    pm.sleep.mode=1 \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    dalvik.vm.profiler=1 \
+    dalvik.vm.isa.arm.features=lpae,div
+
 # Nova Launcher
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/app/NovaLauncher.apk:system/priv-app/NovaLauncher/NovaLauncher.apk
