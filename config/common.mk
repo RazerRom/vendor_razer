cmke/android_vendor_cm

UKM kecinzer edition
UKM for Synapse with my modifications

Change-Id: I33b647e2ec77f41666ae69e9678cb3daa0713751
Signed-off-by: Petr kecinzer Řezníček <kecinzer@gmail.com>

config/common.mk
@@ -344,6 +344,8 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(CM_DISPLAY_VERSION)
 
+-include vendor/cm/config/common_ukm.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 -include vendor/cyngn/product.mk

config/common_ukm.mk
@@ -1,0 +1,76 @@
+# UKM
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/UKM/UKM:system/etc/init.d/UKM \
+    vendor/cm/prebuilt/common/UKM/uci:system/xbin/uci \
+    vendor/cm/prebuilt/common/UKM/busybox:install/UKM/busybox \
+    vendor/cm/prebuilt/common/UKM/actions/boolean:install/UKM/actions/boolean \
+    vendor/cm/prebuilt/common/UKM/actions/bracket-option:install/UKM/actions/bracket-option \
+    vendor/cm/prebuilt/common/UKM/actions/buildprop:install/UKM/actions/buildprop \
+    vendor/cm/prebuilt/common/UKM/actions/cpuboostibf:install/UKM/actions/cpuboostibf \
+    vendor/cm/prebuilt/common/UKM/actions/cpuset:install/UKM/actions/cpuset \
+    vendor/cm/prebuilt/common/UKM/actions/cpuvolt:install/UKM/actions/cpuvolt \
+    vendor/cm/prebuilt/common/UKM/actions/devtools:install/UKM/actions/devtools \
+    vendor/cm/prebuilt/common/UKM/actions/dropcaches:install/UKM/actions/dropcaches \
+    vendor/cm/prebuilt/common/UKM/actions/gamma:install/UKM/actions/gamma \
+    vendor/cm/prebuilt/common/UKM/actions/generic:install/UKM/actions/generic \
+    vendor/cm/prebuilt/common/UKM/actions/gpuset:install/UKM/actions/gpuset \
+    vendor/cm/prebuilt/common/UKM/actions/intellithermal:install/UKM/actions/intellithermal \
+    vendor/cm/prebuilt/common/UKM/actions/ioset:install/UKM/actions/ioset \
+    vendor/cm/prebuilt/common/UKM/actions/live:install/UKM/actions/live \
+    vendor/cm/prebuilt/common/UKM/actions/lmk:install/UKM/actions/lmk \
+    vendor/cm/prebuilt/common/UKM/actions/mpdboostfreq:install/UKM/actions/mpdboostfreq \
+    vendor/cm/prebuilt/common/UKM/actions/numeric:install/UKM/actions/numeric \
+    vendor/cm/prebuilt/common/UKM/actions/powersuspend:install/UKM/actions/powersuspend \
+    vendor/cm/prebuilt/common/UKM/actions/printk:install/UKM/actions/printk \
+    vendor/cm/prebuilt/common/UKM/actions/restorebackup:install/UKM/actions/restorebackup \
+    vendor/cm/prebuilt/common/UKM/actions/serviceset:install/UKM/actions/serviceset \
+    vendor/cm/prebuilt/common/UKM/actions/socset:install/UKM/actions/socset \
+    vendor/cm/prebuilt/common/UKM/actions/soundfaux:install/UKM/actions/soundfaux \
+    vendor/cm/prebuilt/common/UKM/actions/speakerfaux:install/UKM/actions/speakerfaux \
+    vendor/cm/prebuilt/common/UKM/actions/sqlite:install/UKM/actions/sqlite \
+    vendor/cm/prebuilt/common/UKM/actions/touch:install/UKM/actions/touch \
+    vendor/cm/prebuilt/common/UKM/actions/voltage:install/UKM/actions/voltage \
+    vendor/cm/prebuilt/common/UKM/actions/wake:install/UKM/actions/wake \
+    vendor/cm/prebuilt/common/UKM/actions/zram:install/UKM/actions/zram \
+    vendor/cm/prebuilt/common/UKM/debug/last_kmsg:install/UKM/debug/last_kmsg \
+    vendor/cm/prebuilt/common/UKM/debug/pvs_bin:install/UKM/debug/pvs_bin \
+    vendor/cm/prebuilt/common/UKM/debug/speed_bin:install/UKM/debug/speed_bin \
+    vendor/cm/prebuilt/common/UKM/device/.device.sh:install/UKM/device/.device.sh \
+    vendor/cm/prebuilt/common/UKM/device/bacon.sh:install/UKM/device/bacon.sh \
+    vendor/cm/prebuilt/common/UKM/device/deb.sh:install/UKM/device/deb.sh \
+    vendor/cm/prebuilt/common/UKM/device/g2.sh:install/UKM/device/g2.sh \
+    vendor/cm/prebuilt/common/UKM/device/hammerhead.sh:install/UKM/device/hammerhead.sh \
+    vendor/cm/prebuilt/common/UKM/device/maguro.sh:install/UKM/device/maguro.sh \
+    vendor/cm/prebuilt/common/UKM/device/mako.sh:install/UKM/device/mako.sh \
+    vendor/cm/prebuilt/common/UKM/files/bck_prof:install/UKM/files/bck_prof \
+    vendor/cm/prebuilt/common/UKM/files/dropcaches_prof:install/UKM/files/dropcaches_prof \
+    vendor/cm/prebuilt/common/UKM/files/gamma_prof:install/UKM/files/gamma_prof \
+    vendor/cm/prebuilt/common/UKM/files/gammafaux_prof:install/UKM/files/gammafaux_prof \
+    vendor/cm/prebuilt/common/UKM/files/gammafranco_prof:install/UKM/files/gammafranco_prof \
+    vendor/cm/prebuilt/common/UKM/files/gammafrancoN5_prof:install/UKM/files/gammafrancoN5_prof \
+    vendor/cm/prebuilt/common/UKM/files/lmk_prof:install/UKM/files/lmk_prof \
+    vendor/cm/prebuilt/common/UKM/files/sound_prof:install/UKM/files/sound_prof \
+    vendor/cm/prebuilt/common/UKM/files/speaker_prof:install/UKM/files/speaker_prof \
+    vendor/cm/prebuilt/common/UKM/files/volt_prof:install/UKM/files/volt_prof \
+    vendor/cm/prebuilt/common/UKM/files/wake_prof:install/UKM/files/wake_prof \
+    vendor/cm/prebuilt/common/UKM/config.json.generate:install/UKM/config.json.generate \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.adv:install/UKM/config.json.generate.adv \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.bprop:install/UKM/config.json.generate.bprop \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.cpu:install/UKM/config.json.generate.cpu \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.cpuvolt:install/UKM/config.json.generate.cpuvolt \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.error:install/UKM/config.json.generate.error \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.gamma:install/UKM/config.json.generate.gamma \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.gpu:install/UKM/config.json.generate.gpu \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.hotplug:install/UKM/config.json.generate.hotplug \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.info:install/UKM/config.json.generate.info \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.io:install/UKM/config.json.generate.io \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.mem:install/UKM/config.json.generate.mem \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.misc:install/UKM/config.json.generate.misc \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.profile:install/UKM/config.json.generate.profile \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.sound:install/UKM/config.json.generate.sound \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.speaker:install/UKM/config.json.generate.speaker \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.status:install/UKM/config.json.generate.status \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.thermal:install/UKM/config.json.generate.thermal \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.tools:install/UKM/config.json.generate.tools \
+    vendor/cm/prebuilt/common/UKM/config.json.generate.wake:install/UKM/config.json.generate.wake \
+    vendor/cm/prebuilt/common/UKM/uci:install/UKM/uci

RazerRom/vendor_razer

config/common.mk
@@ -239,6 +239,8 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 # BLISS Versioning System
 -include vendor/bliss/config/versions.mk
 
+-include vendor/bliss/config/common_ukm.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 -include vendor/cyngn/product.mk

config/common_ukm.mk
@@ -1,0 +1,76 @@
+# UKM
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/UKM/UKM:system/etc/init.d/UKM \
+    vendor/bliss/prebuilt/common/UKM/uci:system/xbin/uci \
+    vendor/bliss/prebuilt/common/UKM/busybox:install/UKM/busybox \
+    vendor/bliss/prebuilt/common/UKM/actions/boolean:install/UKM/actions/boolean \
+    vendor/bliss/prebuilt/common/UKM/actions/bracket-option:install/UKM/actions/bracket-option \
+    vendor/bliss/prebuilt/common/UKM/actions/buildprop:install/UKM/actions/buildprop \
+    vendor/bliss/prebuilt/common/UKM/actions/cpuboostibf:install/UKM/actions/cpuboostibf \
+    vendor/bliss/prebuilt/common/UKM/actions/cpuset:install/UKM/actions/cpuset \
+    vendor/bliss/prebuilt/common/UKM/actions/cpuvolt:install/UKM/actions/cpuvolt \
+    vendor/bliss/prebuilt/common/UKM/actions/devtools:install/UKM/actions/devtools \
+    vendor/bliss/prebuilt/common/UKM/actions/dropcaches:install/UKM/actions/dropcaches \
+    vendor/bliss/prebuilt/common/UKM/actions/gamma:install/UKM/actions/gamma \
+    vendor/bliss/prebuilt/common/UKM/actions/generic:install/UKM/actions/generic \
+    vendor/bliss/prebuilt/common/UKM/actions/gpuset:install/UKM/actions/gpuset \
+    vendor/bliss/prebuilt/common/UKM/actions/intellithermal:install/UKM/actions/intellithermal \
+    vendor/bliss/prebuilt/common/UKM/actions/ioset:install/UKM/actions/ioset \
+    vendor/bliss/prebuilt/common/UKM/actions/live:install/UKM/actions/live \
+    vendor/bliss/prebuilt/common/UKM/actions/lmk:install/UKM/actions/lmk \
+    vendor/bliss/prebuilt/common/UKM/actions/mpdboostfreq:install/UKM/actions/mpdboostfreq \
+    vendor/bliss/prebuilt/common/UKM/actions/numeric:install/UKM/actions/numeric \
+    vendor/bliss/prebuilt/common/UKM/actions/powersuspend:install/UKM/actions/powersuspend \
+    vendor/bliss/prebuilt/common/UKM/actions/printk:install/UKM/actions/printk \
+    vendor/bliss/prebuilt/common/UKM/actions/restorebackup:install/UKM/actions/restorebackup \
+    vendor/bliss/prebuilt/common/UKM/actions/serviceset:install/UKM/actions/serviceset \
+    vendor/bliss/prebuilt/common/UKM/actions/socset:install/UKM/actions/socset \
+    vendor/bliss/prebuilt/common/UKM/actions/soundfaux:install/UKM/actions/soundfaux \
+    vendor/bliss/prebuilt/common/UKM/actions/speakerfaux:install/UKM/actions/speakerfaux \
+    vendor/bliss/prebuilt/common/UKM/actions/sqlite:install/UKM/actions/sqlite \
+    vendor/bliss/prebuilt/common/UKM/actions/touch:install/UKM/actions/touch \
+    vendor/bliss/prebuilt/common/UKM/actions/voltage:install/UKM/actions/voltage \
+    vendor/bliss/prebuilt/common/UKM/actions/wake:install/UKM/actions/wake \
+    vendor/bliss/prebuilt/common/UKM/actions/zram:install/UKM/actions/zram \
+    vendor/bliss/prebuilt/common/UKM/debug/last_kmsg:install/UKM/debug/last_kmsg \
+    vendor/bliss/prebuilt/common/UKM/debug/pvs_bin:install/UKM/debug/pvs_bin \
+    vendor/bliss/prebuilt/common/UKM/debug/speed_bin:install/UKM/debug/speed_bin \
+    vendor/bliss/prebuilt/common/UKM/device/.device.sh:install/UKM/device/.device.sh \
+    vendor/bliss/prebuilt/common/UKM/device/bacon.sh:install/UKM/device/bacon.sh \
+    vendor/bliss/prebuilt/common/UKM/device/deb.sh:install/UKM/device/deb.sh \
+    vendor/bliss/prebuilt/common/UKM/device/g2.sh:install/UKM/device/g2.sh \
+    vendor/bliss/prebuilt/common/UKM/device/hammerhead.sh:install/UKM/device/hammerhead.sh \
+    vendor/bliss/prebuilt/common/UKM/device/maguro.sh:install/UKM/device/maguro.sh \
+    vendor/bliss/prebuilt/common/UKM/device/mako.sh:install/UKM/device/mako.sh \
+    vendor/bliss/prebuilt/common/UKM/files/bck_prof:install/UKM/files/bck_prof \
+    vendor/bliss/prebuilt/common/UKM/files/dropcaches_prof:install/UKM/files/dropcaches_prof \
+    vendor/bliss/prebuilt/common/UKM/files/gamma_prof:install/UKM/files/gamma_prof \
+    vendor/bliss/prebuilt/common/UKM/files/gammafaux_prof:install/UKM/files/gammafaux_prof \
+    vendor/bliss/prebuilt/common/UKM/files/gammafranco_prof:install/UKM/files/gammafranco_prof \
+    vendor/bliss/prebuilt/common/UKM/files/gammafrancoN5_prof:install/UKM/files/gammafrancoN5_prof \
+    vendor/bliss/prebuilt/common/UKM/files/lmk_prof:install/UKM/files/lmk_prof \
+    vendor/bliss/prebuilt/common/UKM/files/sound_prof:install/UKM/files/sound_prof \
+    vendor/bliss/prebuilt/common/UKM/files/speaker_prof:install/UKM/files/speaker_prof \
+    vendor/bliss/prebuilt/common/UKM/files/volt_prof:install/UKM/files/volt_prof \
+    vendor/bliss/prebuilt/common/UKM/files/wake_prof:install/UKM/files/wake_prof \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate:install/UKM/config.json.generate \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.adv:install/UKM/config.json.generate.adv \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.bprop:install/UKM/config.json.generate.bprop \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.cpu:install/UKM/config.json.generate.cpu \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.cpuvolt:install/UKM/config.json.generate.cpuvolt \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.error:install/UKM/config.json.generate.error \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.gamma:install/UKM/config.json.generate.gamma \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.gpu:install/UKM/config.json.generate.gpu \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.hotplug:install/UKM/config.json.generate.hotplug \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.info:install/UKM/config.json.generate.info \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.io:install/UKM/config.json.generate.io \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.mem:install/UKM/config.json.generate.mem \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.misc:install/UKM/config.json.generate.misc \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.profile:install/UKM/config.json.generate.profile \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.sound:install/UKM/config.json.generate.sound \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.speaker:install/UKM/config.json.generate.speaker \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.status:install/UKM/config.json.generate.status \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.thermal:install/UKM/config.json.generate.thermal \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.tools:install/UKM/config.json.generate.tools \
+    vendor/bliss/prebuilt/common/UKM/config.json.generate.wake:install/UKM/config.json.generate.wake \
+    vendor/bliss/prebuilt/common/UKM/uci:install/UKM/uci