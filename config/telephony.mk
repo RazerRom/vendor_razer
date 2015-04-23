CyanogenMod/android_vendor_cm

Re-add the fixes from Remicks.
Change-Id: Ia8ff39878203d938624d1bbc9c072dc8900261f6

config/telephony.mk
@@ -25,4 +25,4 @@ PRODUCT_PACKAGES += \
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg
+    ro.config.ringtone=Highscore.ogg

RazerRom/vendor_razer

config/telephony.mk
@@ -23,4 +23,4 @@ PRODUCT_PACKAGES += \
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg
+    ro.config.ringtone=Triton.ogg