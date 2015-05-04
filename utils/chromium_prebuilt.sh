1bd6e077c01ee9debdba0b5ca9068ef522e0bd07
abun880007 <tigger2014@team-ub.co.uk>
2015-05-04 13:39:15 +0100

    chromium_prebuilt for 64bit
    Change-Id: I6bd3c7464faab86cc36a7d55cbb159c71e5819bb
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- utils/chromium_prebuilt.sh
+++ utils/chromium_prebuilt.sh
@@ -34,51 +34,116 @@ if [ -d $PREBUILT_DIR ]; then
     rm -rf $PREBUILT_DIR
 fi
 
-mkdir -p $PREBUILT_DIR
-mkdir -p $PREBUILT_DIR/app
-mkdir -p $PREBUILT_DIR/lib
+if [ -d $TARGET_DIR/system/lib64 ]; then
 
-if [ -d $TARGET_DIR ]; then
-    echo "Copying files..."
-    cp -r $TARGET_DIR/system/app/webview $PREBUILT_DIR/app
-    cp $TARGET_DIR/system/lib/libwebviewchromium.so $PREBUILT_DIR/lib/libwebviewchromium.so
-    cp $TARGET_DIR/system/lib/libwebviewchromium_plat_support.so $PREBUILT_DIR/lib/libwebviewchromium_plat_support.so
-    cp $TARGET_DIR/system/lib/libwebviewchromium_loader.so $PREBUILT_DIR/lib/libwebviewchromium_loader.so
-else
-    echo "Please ensure that you have ran a full build prior to running this script!"
-    return 1;
-fi
+    mkdir -p $PREBUILT_DIR
+    mkdir -p $PREBUILT_DIR/app
+    mkdir -p $PREBUILT_DIR/lib
+    mkdir -p $PREBUILT_DIR/lib64
 
-echo "Generating Makefiles..."
+    if [ -d $TARGET_DIR ]; then
+        echo "Copying files..."
+        cp -r $TARGET_DIR/system/app/webview $PREBUILT_DIR/app
+        cp $TARGET_DIR/system/lib/libwebviewchromium.so $PREBUILT_DIR/lib/libwebviewchromium.so
+        cp $TARGET_DIR/system/lib/libwebviewchromium_plat_support.so $PREBUILT_DIR/lib/libwebviewchromium_plat_support.so
+        cp $TARGET_DIR/system/lib/libwebviewchromium_loader.so $PREBUILT_DIR/lib/libwebviewchromium_loader.so
+        #Copy 64 bit libs
+        cp $TARGET_DIR/system/lib64/libwebviewchromium.so $PREBUILT_DIR/lib64/libwebviewchromium.so
+        cp $TARGET_DIR/system/lib64/libwebviewchromium_plat_support.so $PREBUILT_DIR/lib64/libwebviewchromium_plat_support.so
+        cp $TARGET_DIR/system/lib64/libwebviewchromium_loader.so $PREBUILT_DIR/lib64/libwebviewchromium_loader.so
+    else
+        echo "Please ensure that you have ran a full build prior to running this script!"
+        return 1;
+    fi
 
-HASH=$(git --git-dir=$TOP/external/chromium_org/.git --work-tree=$TOP/external/chromium_org rev-parse --verify HEAD)
-echo $HASH > $PREBUILT_DIR/hash.txt
+    echo "Generating Makefiles..."
 
-sed s/__DEVICE__/$DEVICE/g > $PREBUILT_DIR/chromium_prebuilt.mk << EOF
-# Copyright (C) 2014 The OmniROM Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+    HASH=$(git --git-dir=$TOP/external/chromium_org/.git --work-tree=$TOP/external/chromium_org rev-parse --verify HEAD)
+    echo $HASH > $PREBUILT_DIR/hash.txt
 
-LOCAL_PATH := prebuilts/chromium/__DEVICE__/
+    sed s/__DEVICE__/$DEVICE/g > $PREBUILT_DIR/chromium_prebuilt.mk << EOF
+    # Copyright (C) 2014 The OmniROM Project
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    # http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
 
-PRODUCT_COPY_FILES += \\
-    \$(LOCAL_PATH)/app/webview/webview.apk:system/app/webview/webview.apk \\
-    \$(LOCAL_PATH)/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \\
-    \$(LOCAL_PATH)/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \\
-    \$(LOCAL_PATH)/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so
+    LOCAL_PATH := prebuilts/chromium/__DEVICE__/
 
-\$(shell mkdir -p out/target/product/__DEVICE__/system/app/webview/lib/arm/)
-\$(shell cp -r \$(LOCAL_PATH)/app/webview/lib/arm/libwebviewchromium.so out/target/product/__DEVICE__/system/app/webview/lib/arm/libwebviewchromium.so)
+    PRODUCT_COPY_FILES += \\
+        \$(LOCAL_PATH)/app/webview/webview.apk:system/app/webview/webview.apk \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so \\
+        \$(LOCAL_PATH)/lib64/libwebviewchromium.so:system/lib64/libwebviewchromium.so \\
+        \$(LOCAL_PATH)/lib64/libwebviewchromium_plat_support.so:system/lib64/libwebviewchromium_plat_support.so \\
+        \$(LOCAL_PATH)/lib64/libwebviewchromium_loader.so:system/lib64/libwebviewchromium_loader.so
+
+    \$(shell mkdir -p out/target/product/__DEVICE__/system/app/webview/lib/arm/)
+    \$(shell mkdir -p out/target/product/__DEVICE__/system/app/webview/lib/arm64/)
+    \$(shell cp -r \$(LOCAL_PATH)/app/webview/lib/arm/libwebviewchromium.so out/target/product/__DEVICE__/system/app/webview/lib/arm/libwebviewchromium.so)
+    \$(shell cp -r \$(LOCAL_PATH)/app/webview/lib/arm64/libwebviewchromium.so out/target/product/__DEVICE__/system/app/webview/lib/arm64/libwebviewchromium.so)
 
 EOF
-echo "Done!"
+    echo "CHROMIUM created as 64-bit."
+
+else
+
+    mkdir -p $PREBUILT_DIR
+    mkdir -p $PREBUILT_DIR/app
+    mkdir -p $PREBUILT_DIR/lib
+
+    if [ -d $TARGET_DIR ]; then
+        echo "Copying files..."
+        cp -r $TARGET_DIR/system/app/webview $PREBUILT_DIR/app
+        cp $TARGET_DIR/system/lib/libwebviewchromium.so $PREBUILT_DIR/lib/libwebviewchromium.so
+        cp $TARGET_DIR/system/lib/libwebviewchromium_plat_support.so $PREBUILT_DIR/lib/libwebviewchromium_plat_support.so
+        cp $TARGET_DIR/system/lib/libwebviewchromium_loader.so $PREBUILT_DIR/lib/libwebviewchromium_loader.so
+    else
+        echo "Please ensure that you have ran a full build prior to running this script!"
+        return 1;
+    fi
+
+    echo "Generating Makefiles..."
+
+    HASH=$(git --git-dir=$TOP/external/chromium_org/.git --work-tree=$TOP/external/chromium_org rev-parse --verify HEAD)
+    echo $HASH > $PREBUILT_DIR/hash.txt
+
+    sed s/__DEVICE__/$DEVICE/g > $PREBUILT_DIR/chromium_prebuilt.mk << EOF
+    # Copyright (C) 2014 The OmniROM Project
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    # http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+
+    LOCAL_PATH := prebuilts/chromium/__DEVICE__/
+
+    PRODUCT_COPY_FILES += \\
+        \$(LOCAL_PATH)/app/webview/webview.apk:system/app/webview/webview.apk \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \\
+        \$(LOCAL_PATH)/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so
+
+    \$(shell mkdir -p out/target/product/__DEVICE__/system/app/webview/lib/arm/)
+    \$(shell cp -r \$(LOCAL_PATH)/app/webview/lib/arm/libwebviewchromium.so out/target/product/__DEVICE__/system/app/webview/lib/arm/libwebviewchromium.so)
+
+EOF
+    echo "CHROMIUM created as 32-bit."
+fi
+
