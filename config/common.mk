0e86d735f093a2aa8ce738ae5806cefc8aeb47d2
Adnan Begovic <adnan@cyngn.com>
2015-06-04 22:23:00 -0400

    vendor/cm: Create CM_PLATFORM_SDK_VERSION.
    Change-Id: I9ea88a865b42875e7c3a1b533f440a04af343362
    
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -271,6 +271,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.razer.version=$(RAZER_VERSION)
+
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 1
+endif
+
+# CyanogenMod Platform SDK Version
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
+
+-include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+-include vendor/cyngn/product.mk
+
+$(call prepend-product-if-exists, vendor/extra/product.mk)
     
 #
 # RazerRom Extras
